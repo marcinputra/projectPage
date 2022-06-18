cbe78009860d705399c8b6fa78144ebe0e4b7afa
Marcin Putra <marcin.putra@gmail.com>
2022-06-18 12:32:42 +0200

    Pierwsze zmiany - uruchomienie serwera

--- projectPage.js
+++ projectPage.js
@@ -1,1 +1,26 @@
-const express = require('express');
+const express = require('express')
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+// niestandardowa strona 404
+app.use((req, res) => {
+    res.type('text/plain')
+    res.status(404)
+    res.send('404 - nie znaleziono')
+})
+
+// niestandradowa strona 500
+app.use((err, req, res, next) => {
+    console.error(err.message)
+    res.type('text/plain')
+    res.status(500)
+    res.send('500 - błąd serwera')
+})
+
+// informacja w konsoli pod którem portem otworzyliśmy serwer HTTP
+app.listen(port, () => console.log(
+    `Express został uruchomony pod adresem http:///localhost:${port}; `+
+    `Naciśnij Ctrl-C, aby zakończyć.`
+))
