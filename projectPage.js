d24a0fc58a8c4619d5da11f8b4c2c8ba32a7dfc7
Marcin Putra <marcin.putra@gmail.com>
2022-06-19 22:25:46 +0200

    Dodawanie plików statycznych

--- projectPage.js
+++ projectPage.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+// obsługa statycznych plików, dodajemy folder ./public
+app.use(express.static( __dirname + '/public' ))
+
+
 
 // konfiguacja silnika widoków 
 // instalacja poprzez komendę: npm install express-handlebars
