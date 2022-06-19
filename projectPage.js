3969d6867011a301521f86bf92f09d0f5852e6c5
Marcin Putra <marcin.putra@gmail.com>
2022-06-19 22:13:29 +0200

    Dodanie i konfiguracja silnika widokow handlebars

--- projectPage.js
+++ projectPage.js
@@ -1,22 +1,32 @@
 const express = require('express')
-
 const app = express()
-
 const port = process.env.PORT || 3000
 
-// niestandardowa strona 404
+
+// konfiguacja silnika widoków 
+// instalacja poprzez komendę: npm install express-handlebars
+// dodanie silnika widoków
+const { engine } = require('express-handlebars')
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views','./views')
+
+
+// // strona główna i podstrony
+app.get('/',(req,res) => {res.render('home')})
+app.get('/about',(req,res) => {res.render('about')})
+
+// // niestandardowa strona 404
 app.use((req, res) => {
-    res.type('text/plain')
     res.status(404)
-    res.send('404 - nie znaleziono')
+    res.render('404')
 })
 
-// niestandradowa strona 500
+// // niestandradowa strona 500
 app.use((err, req, res, next) => {
     console.error(err.message)
-    res.type('text/plain')
     res.status(500)
-    res.send('500 - błąd serwera')
+    res.render('500')
 })
 
 // informacja w konsoli pod którem portem otworzyliśmy serwer HTTP
