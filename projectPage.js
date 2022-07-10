9a2ba1038153a2abcc54894bac20f399494313e5
Marcin Putra <marcin.putra@gmail.com>
2022-07-10 17:29:12 +0200

    QA - testowanie kodu, lintowanie

--- projectPage.js
+++ projectPage.js
@@ -1,4 +1,4 @@
-const fortune = require('./lib/fortunerandom')
+const handlers = require('./lib/handlers')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
@@ -16,29 +16,44 @@ app.set('views','./views')
 
 
 // losowanie z pliku fortunerandom.js
-app.get('/random',(req,res) => {
-    res.render('random',{fortune: fortune.getFortuneRandom() })
-})
+// app.get('/random',(req,res) => {
+//     res.render('random',{fortune: fortune.getFortuneRandom() })
+// })
+app.get('/random', handlers.randomFortune )
 
 // strona główna i podstrony
-app.get('/',(req,res) => {res.render('home')})
-app.get('/about',(req,res) => {res.render('about')})
+// app.get('/',(req,res) => {res.render('home')})
+app.get('/',handlers.home)
+// app.get('/about',(req,res) => {res.render('about')})
+app.get( '/about',handlers.about )
 
 // niestandardowa strona 404
-app.use((req, res) => {
-    res.status(404)
-    res.render('404')
-})
+// app.use((req, res) => {
+//     res.status(404)
+//     res.render('404')
+// })
+app.use( handlers.notfound )
 
 // niestandradowa strona 500
-app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
-})
+// app.use((err, req, res, next) => {
+//     console.error(err.message)
+//     res.status(500)
+//     res.render('500')
+// })
+app.use( handlers.serverError)
 
 // informacja w konsoli pod którem portem otworzyliśmy serwer HTTP
-app.listen(port, () => console.log(
-    `Express został uruchomony pod adresem http:///localhost:${port}; `+
-    `Naciśnij Ctrl-C, aby zakończyć.`
-))
+// app.listen(port, () => console.log(
+//     `Express został uruchomony pod adresem http:///localhost:${port}; `+
+//     `Naciśnij Ctrl-C, aby zakończyć.`
+// ))
+
+// zmiana .listen na potrzeby testów puppeteer
+if(require.main == module) {
+    app.listen(port, () => {
+        console.log(`Express został uruchomony pod adresem http:///localhost:${port}; `+
+        `Naciśnij Ctrl-C, aby zakończyć.`)
+    }) 
+} else {
+    module.exports = app
+} 
